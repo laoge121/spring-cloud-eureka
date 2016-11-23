451030799d4a31c9c4885cebd591b166693450f2
yuhou <yuhou@mogujie.com>
2016-11-23 22:55:24 +0800

    自己练习代码

--- spring-cloud-config/src/main/java/com/cloud/config/ApplicationConfig.java
+++ spring-cloud-config/src/main/java/com/cloud/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.cloud.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ApplicationConfig {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ApplicationConfig.class).web(true).run(args);
